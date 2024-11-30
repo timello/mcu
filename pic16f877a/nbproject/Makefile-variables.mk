444aadb233b0c9751aa57399c9d74e528c16ca2e
Tiago Mello <timello@gmail.com>
2024-11-30 12:48:18 +0100

    Rename project

--- pic16f877a/nbproject/Makefile-variables.mk
+++ pic16f877a/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Serial.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/Serial.X.production.hex
+CND_ARTIFACT_NAME_default=pic16f877a.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/pic16f877a.production.hex
